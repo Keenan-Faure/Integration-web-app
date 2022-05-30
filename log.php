8feabc936fbaf011832e63920fb846b252766f2c
Keenan-Faure <keenan.asterisk@gmail.com>
2022-05-30 11:09:18 +0200

    Updated log.php json output

--- log.php
+++ log.php
@@ -1,5 +1,11 @@
 
 <?php
 session_start();
-echo(json_encode($_SESSION['log']));
+$variable = new \stdClass();
+for($i = 0; $i < sizeof($_SESSION['log']); ++$i)
+{
+    $index = $i + 1;
+    $variable->$index = $_SESSION['log'][$i];
+}
+echo(json_encode($variable));
 ?>
