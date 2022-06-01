b5412fdee6580c22281b278ec0bf8c9c3a625bdc
Keenan-Faure <keenan.asterisk@gmail.com>
2022-06-01 15:15:52 +0200

    created API.php to handle endpoints.php

---
+++ API.php
@@ -0,0 +1,64 @@
+<?php
+
+if(isset($_POST['checkConnection']))
+{
+    echo("yes indeed");
+    unset($_POST['checkConnection']);
+}
+if(isset($_POST['viewLog']))
+{
+    echo("yes indeed");
+    unset($_POST['viewLog']);
+}
+if(isset($_POST['vistiS2S']))
+{
+    echo("yes indeed");
+    unset($_POST['visitS2S']);
+}
+if(isset($_POST['getProductBySKU']))
+{
+    echo("yes indeed");
+    unset($_POST['getProductBySKU']);
+}
+if(isset($_POST['getProductsBatch']))
+{
+    echo("yes indeed");
+    unset($_POST['getProductsBatch']);
+}
+if(isset($_POST['countProduct']))
+{
+    echo("yes indeed");
+    unset($_POST['countProduct']);
+}
+if(isset($_POST['viewProductSql']))
+{
+    echo("yes indeed");
+    unset($_POST['viewProductSql']);
+}
+if(isset($_POST['addProduct']))
+{
+    echo("yes indeed");
+    unset($_POST['addProduct']);
+}
+if(isset($_POST['getCustomerByName']))
+{
+    echo("yes indeed");
+    unset($_POST['getCustomerByName']);
+}
+if(isset($_POST['countCustomer']))
+{
+    echo("yes indeed");
+    unset($_POST['countCustomer']);
+}
+if(isset($_POST['viewCustomerSql']))
+{
+    echo("yes indeed");
+    unset($_POST['viewCustomerSql']);
+}
+if(isset($_POST['addCustomer']))
+{
+    echo("yes indeed");
+    unset($_POST['addCustomer']);
+}
+
+?>
