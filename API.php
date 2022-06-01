5a04d6590bea4d6c136dce101b368599e98168a3
Keenan-Faure <keenan.asterisk@gmail.com>
2022-06-01 14:36:01 +0200

    new file for handling api requests of endpoint.php

---
+++ API.php
@@ -0,0 +1,74 @@
+<?php
+
+if(isset($_POST['CheckConnection']))
+{
+    echo("yes indeed");
+    unset($_POST['CheckConnection']);
+}
+if(isset($_POST['CheckConnection']))
+{
+    echo("yes indeed");
+    unset($_POST['CheckConnection']);
+}
+if(isset($_POST['CheckConnection']))
+{
+    echo("yes indeed");
+    unset($_POST['CheckConnection']);
+}
+if(isset($_POST['CheckConnection']))
+{
+    echo("yes indeed");
+    unset($_POST['CheckConnection']);
+}
+if(isset($_POST['CheckConnection']))
+{
+    echo("yes indeed");
+    unset($_POST['CheckConnection']);
+}
+if(isset($_POST['CheckConnection']))
+{
+    echo("yes indeed");
+    unset($_POST['CheckConnection']);
+}
+if(isset($_POST['CheckConnection']))
+{
+    echo("yes indeed");
+    unset($_POST['CheckConnection']);
+}
+if(isset($_POST['CheckConnection']))
+{
+    echo("yes indeed");
+    unset($_POST['CheckConnection']);
+}
+if(isset($_POST['CheckConnection']))
+{
+    echo("yes indeed");
+    unset($_POST['CheckConnection']);
+}
+if(isset($_POST['CheckConnection']))
+{
+    echo("yes indeed");
+    unset($_POST['CheckConnection']);
+}
+if(isset($_POST['CheckConnection']))
+{
+    echo("yes indeed");
+    unset($_POST['CheckConnection']);
+}
+if(isset($_POST['CheckConnection']))
+{
+    echo("yes indeed");
+    unset($_POST['CheckConnection']);
+}
+if(isset($_POST['CheckConnection']))
+{
+    echo("yes indeed");
+    unset($_POST['CheckConnection']);
+}
+if(isset($_POST['CheckConnection']))
+{
+    echo("yes indeed");
+    unset($_POST['CheckConnection']);
+}
+
+?>
