1b6840c949b773953559367682099ead8c0d3cae
Keenan-Faure <keenan.asterisk@gmail.com>
2022-07-21 16:07:03 +0200

    updates

--- editProduct.php
+++ editProduct.php
@@ -4,8 +4,15 @@
     </head>
     <body>
         <div class="bg">
-            <textarea>
-            </textarea>
+            <div class='bg-modalContainer'>
+            <div class="overlay">
+                <h1>Edit Product</h1>
+                <div class='line'></div>
+            </div>
+            <form method='post' action='process.php' target='_blank' id="simple">
+                <textarea></textarea>
+            </form>
+            </div>
         </div>
 
     </body>
