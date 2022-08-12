79e4a7038100c88a7342a44b9af90373367154e9
Keenan Faure <97687673+Keenan-Faure@users.noreply.github.com>
2022-08-12 15:30:45 +0200

    Update cURL.php

--- cURL/cURL.php
+++ cURL/cURL.php
@@ -32,37 +32,38 @@ Class CURL
             }
         }
     }
-    function cURLRequest($page)
+    function get_web_page($url, $uname, $psw) 
+    {
+        $options = array(
+            CURLOPT_USERPWD => $uname . ":" . $psw, 
+            CURLOPT_HTTPHEADER => array("Content-Type: application/json"),
+            CURLOPT_RETURNTRANSFER => true,   // return web page
+            CURLOPT_HEADER         => false,  // don't return headers
+            CURLOPT_FOLLOWLOCATION => true,   // follow redirects
+            CURLOPT_MAXREDIRS      => 10,     // stop after 10 redirects
+            CURLOPT_ENCODING       => "",     // handle compressed
+            CURLOPT_USERAGENT      => "test", // name of client
+            CURLOPT_AUTOREFERER    => true,   // set referrer on redirect
+            CURLOPT_CONNECTTIMEOUT => 120,    // time-out on connect
+            CURLOPT_TIMEOUT        => 120,    // time-out on response
+        ); 
+    
+        $ch = curl_init($url);
+        curl_setopt_array($ch, $options);
+    
+        $content  = curl_exec($ch);
+    
+        curl_close($ch);
+    
+        return $content;
+    }
+    function cURLRequest($page, $request)
     {
         $page = geturl() . "/MySQL-API/API/v1.php";
         $response = get_web_page($page, $username, $password);
         $resArr = array();
         $resArr = json_decode($response);
         echo "<pre>"; print_r($resArr); echo "</pre>";
-        
-        function get_web_page($url, $uname, $psw) {
-            $options = array(
-                CURLOPT_USERPWD => $uname . ":" . $psw, 
-                CURLOPT_RETURNTRANSFER => true,   // return web page
-                CURLOPT_HEADER         => false,  // don't return headers
-                CURLOPT_FOLLOWLOCATION => true,   // follow redirects
-                CURLOPT_MAXREDIRS      => 10,     // stop after 10 redirects
-                CURLOPT_ENCODING       => "",     // handle compressed
-                CURLOPT_USERAGENT      => "test", // name of client
-                CURLOPT_AUTOREFERER    => true,   // set referrer on redirect
-                CURLOPT_CONNECTTIMEOUT => 120,    // time-out on connect
-                CURLOPT_TIMEOUT        => 120,    // time-out on response
-            ); 
-        
-            $ch = curl_init($url);
-            curl_setopt_array($ch, $options);
-        
-            $content  = curl_exec($ch);
-        
-            curl_close($ch);
-        
-            return $content;
-        }
     }
 }
 ?>
