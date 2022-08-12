24832bfa9c844481402083035def7fd33a29c049
Keenan Faure <97687673+Keenan-Faure@users.noreply.github.com>
2022-08-12 15:30:37 +0200

    Create cURL template.php

---
+++ cURL/cURL template.php
@@ -0,0 +1,82 @@
+
+<?php
+// Install:
+// composer require automattic/woocommerce
+
+// Setup:
+require __DIR__ . '/vendor/autoload.php';
+
+use Automattic\WooCommerce\Client;
+
+$woocommerce = new Client(
+    'http://example.com', // Your store URL
+    'consumer_key', // Your consumer key
+    'consumer_secret', // Your consumer secret
+    [
+        'wp_api' => true, // Enable the WP REST API integration
+        'version' => 'wc/v3' // WooCommerce WP REST API version
+    ]
+);
+?>
+
+
+<?php
+//PHP VERSION
+$data = [
+    'name' => 'Premium Quality',
+    'type' => 'simple',
+    'regular_price' => '21.99',
+    'description' => 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.',
+    'short_description' => 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.',
+    'categories' => [
+        [
+            'id' => 9
+        ],
+        [
+            'id' => 14
+        ]
+    ],
+    'images' => [
+        [
+            'src' => 'http://demo.woothemes.com/woocommerce/wp-content/uploads/sites/56/2013/06/T_2_front.jpg'
+        ],
+        [
+            'src' => 'http://demo.woothemes.com/woocommerce/wp-content/uploads/sites/56/2013/06/T_2_back.jpg'
+        ]
+    ]
+];
+
+print_r($woocommerce->post('products', $data));
+?>
+
+<?php
+//CURL RESPONSE EXAMPLE
+curl -X POST https://example.com/wp-json/wc/v3/products \
+    -u consumer_key:consumer_secret \
+    -H "Content-Type: application/json" \
+    -d '{
+  "name": "Premium Quality",
+  "type": "simple",
+  "regular_price": "21.99",
+  "description": "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.",
+  "short_description": "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.",
+  "categories": [
+    {
+      "id": 9
+    },
+    {
+      "id": 14
+    }
+  ],
+  "images": [
+    {
+      "src": "http://demo.woothemes.com/woocommerce/wp-content/uploads/sites/56/2013/06/T_2_front.jpg"
+    },
+    {
+      "src": "http://demo.woothemes.com/woocommerce/wp-content/uploads/sites/56/2013/06/T_2_back.jpg"
+    }
+  ]
+}'
+
+?>
+
