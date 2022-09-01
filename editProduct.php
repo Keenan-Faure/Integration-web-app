03ac852c0ab62a72198c00d3344cfd3309ab3d58
Keenan-Faure <keenan.asterisk@gmail.com>
2022-09-01 15:21:19 +0200

    Update

--- editProduct.php
+++ editProduct.php
@@ -79,7 +79,7 @@
             $sku = $_SESSION['products']->result[$i]->SKU;
             $active = $_SESSION['products']->result[$i]->Active;
             $brand = $_SESSION['products']->result[$i]->Brand;
-            echo("<script>createPLV('". $active . "','" . $sku . "','" . $title . "','" . $brand . "','" . $sku . "','" . $sku ."');</script>");
+            echo("<script>createPLV('". $active . "','" . $sku . "','" . $title . "','" . $brand . "','" . $sku . "','" . $sku . "');</script>");
         }
         $_SESSION['products'] = sizeof($_SESSION['products']->result);
     ?>
