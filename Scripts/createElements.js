380073046a0a3429ba12b32864e5e3d52eda2cf9
Keenan-Faure <keenan.asterisk@gmail.com>
2022-09-14 12:42:45 +0200

    Images, htm/php for newView

--- Scripts/createElements.js
+++ Scripts/createElements.js
@@ -54,7 +54,7 @@ function createPLV(t1, t2, t3, t4, sku, skuValue)
     */
     let form = document.createElement('form');
     form.method = 'post';
-    form.action = 'editProducts.php';
+    form.action = 'newView.php';
     let div = document.createElement('button');
 
     div.className = 'fullsize';
@@ -167,4 +167,77 @@ function createSumbit()
 
     form = document.getElementById('form');
     form.appendChild(button);
+}
+
+
+//Run-format of the functions
+//getClassNames -> convertJsonToArray -> setText (Filters) -> applyText
+
+
+//id is the id of the element
+//either variant or general
+function convertJsonToArray(texter)
+{
+    //converts json object into an array
+    let result = [];
+    for(let i in texter)
+    {
+        result.push([i, texter[i]]);
+    }
+    return result;
+}
+function getClassNames(text)
+{
+    $(document).ready(()=>
+    {
+        let generalClassNames = [null, null, null, 's', 'titleContainer', 'longDescriptionContainer', 'pc', 'cl', 'pt', 'vd', 'vc', 'bc', 
+        'wv', 'ctp', 'sp', 'q', 'on1', 'ov1', 'on2', 'ov2', 'm1', 'm2', 'm3'];
+        let valueArray = convertJsonToArray(text);
+        //console.log(valueArray[valueArray.length - 1]); //23
+        //console.log(generalClassNames.length); //20
+        setText(generalClassNames, valueArray);
+    });
+}
+
+//applies a text node to a certain element
+//className => is the name of the element in HTML
+//text => is the array containing the values that will be  assigned to the element
+function applyText(className, Text)
+{
+    //queries className
+    let object = document.querySelector('.' + className);
+    
+    //creates text node
+    let text = document.createTextNode(Text);
+    //objects text to parent
+    object.appendChild(text);
+}
+
+//Uses parallel arrays to loop through the text array
+//If it's values are part of the ignore array
+//Then skip that iteration
+//otherwise add the text to the class
+function setText(classNames, text)
+{
+    let ignore = ['Token', 'Type', 'Active'];
+    for(let i = 0; i < text.length; ++i)
+    {
+        if(ignore.includes(text[i][0]))
+        {
+            continue;
+        }
+        else if(text[i][0] == 'Description')
+        {
+            document.querySelector('.' + classNames[i]).insertAdjacentHTML("afterbegin", text[i][1]);
+            continue;
+        }
+        else
+        {
+            if(text[i][1] == null || text[i][1] == '')
+            {
+                text[i][1] = null;
+            }
+            applyText(classNames[i], text[i][1]);
+        }
+    }
 }
---
+++ newView.php
@@ -0,0 +1,166 @@
+<?php 
+    
+    session_start(); 
+    include("createConnection.php");
+    use Connection\Connection as connect;
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="icon" type="image/x-icon" href="Images/logo.png"/>
+        <link rel='stylesheet' href='Styles/viewItem.css'>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+        <script src="Scripts/fade.js"></script>
+        <script src="Scripts/createElements.js"></script>
+        <?php 
+        
+        if(isset($_POST))
+        {
+            $sku = array_keys($_POST)[0];
+            $sku = ltrim(rtrim($_POST[$sku]));
+            unset($_POST);
+
+            if($_SESSION['connection'])
+            {
+                $connection2 = new connect();
+                $rawConnection = $connection2->createConnection($_SESSION['credentials']->username, $_SESSION['credentials']->password, 'localhost', $_SESSION['connection']->credentials->dbname)->rawValue;
+                $query2 = 'select * from Inventory where SKU = "' . $sku .  '"';
+                $output2 = $connection2->converterObject($rawConnection, $query2, $_SESSION['connection']->credentials->dbname);
+                $body_html = 'Description';
+                $output2->result[0]->$body_html = html_entity_decode($output2->result[0]->$body_html);
+                $result = json_encode($output2->result[0]);
+            }
+            //passes the text as a json object
+            echo("<script>getClassNames($result);</script>");
+        }
+        
+        ?>
+    </head>
+    <body>
+        <div class='backgroundtwo'>
+        </div>
+        <!-- NavBar -->
+        <div class="navBar">
+            <div class="overlay">
+                <div class='imageNav'></div>
+                <h1>Simple Product</h1>
+                <div class='buttonContainer2'>
+                    <div class="dropDown">
+                    <button class="dropDownBtn">Home</button>
+                        <div class="dropDownContent">
+                            <a href="serverData.php">Dashboard</a>
+                        </div>
+                    </div>
+
+                    <div class="dropDown">
+                    <button class="dropDownBtn">Endpoints</button>
+                        <div class="dropDownContent">
+                            <a href="endpoints.php">View Endpoints</a>
+                            <a href="API/document.html" target='_blank'>API</a>
+                        </div>
+                    </div>
+                </div>
+                <div class='buttonContainer'>
+                    <div class="dropDown">
+                    <button class="dropDownBtn">Products</button>
+                        <div class="dropDownContent">
+                            <a href="addItem.html">Add Product</a>
+                            <a href="editProduct.php">View all products</a>
+                        </div>
+                    </div>
+
+                    <div class="dropDown">
+                    <button class="dropDownBtn">Customers</button>
+                        <div class="dropDownContent">
+                            <a href="addCustomer.html">Add Customer</a>
+                            <a href="editCustomer.php">View Customers</a>
+                        </div>
+                    </div>
+                </div>
+                <a href="importUtils/import.html" class="buttonOption"></a>
+
+            </div>
+        </div>
+
+        <!-- Content -->
+        <!-- General -->
+        <div class="content">
+            <div class="General">
+                <div class="vData" id="vDatad"><p>General</p></div>
+                <div class="vData" id="gData"><p>Variant</p></div>
+            </div>
+            <div class="GeneralContentContainer" id="general">
+                <div class="imageContainer"></div>
+                <div class="titleContainer"></div>
+                <div class="generalInfoContainer">
+                    <div id="gap">
+                        <div class="dataContainer">
+                            Vendor
+                        </div>
+                        <div class="dataValue vd"></div>
+                    </div>
+
+                    <div id="gap">
+                        <div class="dataContainer">
+                            Collection
+                        </div>
+                        <div class="dataValue cl"></div>
+                    </div>
+                    
+                    <div id="gap">
+                        <div class="dataContainer">
+                            Product Type
+                        </div>
+                        <div class="dataValue pt"></div>
+                    </div>
+                </div>
+                <div class="metaHeader">Attributes</div>
+                <div class="Meta">
+                    <div class="meta1 m1"></div>
+                    <div class="meta1 m2"></div>
+                    <div class="meta1 m3"></div>
+                </div>
+                <div class="longDescHeader">Long Description</div>
+                <div class="longDescriptionContainer"></div>
+            </div>
+            <!-- Variant -->
+            <div class="GeneralContentContainer" id="variant">
+                <div class="variantData">
+                    <div class="headers">SKU</div>
+                    <div class="sku s"></div>
+                    <br><br>
+                    <div class="headers">Variant Code</div>
+                    <div class="sku vc"></div>
+                    <br><br>
+                    <div class="headers">Product Code</div>
+                    <div class="sku pc"></div>
+                    <br><br>
+                    <div class="headers">Barcode</div>
+                    <div class="sku bc"></div>
+                </div>
+                <div class="priceQuantityHeader">Price and Quantity</div>
+                <div class="priceQuantity">
+                    <div class="priceQ">Compare To Price</div>
+                    <div class="priceQValue ctp"></div>
+                    <div class="priceQ">Selling Price</div>
+                    <div class="priceQValue sp"></div>
+                    <br><br>
+                    <div class="priceQ ">Cape Town Warehouse</div>
+                    <div class="priceQValue q"></div>
+                </div>
+                <div class="weightHeader">Weight (grams)</div>
+                <div class="weight">
+                    <div class="weighter">Weight</div>
+                    <div class="weightValue wv"></div>
+                </div>
+                <div class="optionHeader">Options</div>
+                <div class="Option">
+                    <div class="option1 on1"></div>
+                    <div class="option1 on2"></div>
+                    <div class="option1 ov1"></div>
+                    <div class="option1 ov2"></div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
