76210311b6bd4411a699d7ee15d70e64200ecb6d
Keenan-Faure <keenan.asterisk@gmail.com>
2022-09-22 13:28:27 +0200

    added fade function in script

--- Scripts/app.js
+++ Scripts/app.js
@@ -2,6 +2,15 @@ $(document).ready(()=>
 {
     $('#push').click(()=>
     {
-        
+        console.log("I am here");
+        console.log($('.conditions').css('opacity') == '0');
+        if($('.conditions').css('opacity') == '0')
+        {
+            $('.conditions').fadeTo('slow', 1);
+        }
+        else
+        {
+            $('.conditions').fadeTo('slow', 0);
+        }
     });
 });
