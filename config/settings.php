a1c7d1a2a1264d367fa6264ff0f84c983fa5d0c6
Keenan-Faure <keenan.asterisk@gmail.com>
2022-11-01 16:18:20 +0200

    restructured the settings.php

--- config/settings.php
+++ config/settings.php
@@ -7,23 +7,27 @@
 
 return array(
 
-    //S2S User credentials
-    's2s_user' => 'keenan.faure',
-    's2s_password' => 'Re_Ghoul',
+    'Stock2Shop Credentials' =>
+    array(
+        's2s_user' => 'keenan.faure',
+        's2s_password' => 'Re_Ghoul'),
 
-    //S2S settings
-    's2s_addProducts' => 'true',
-    's2s_deleteProducts' => 'true',
-    's2s_productMap' => '{}',
+    'S2S settings' =>
+    array(
+        's2s_add_products' => 'true',
+        's2s_delete_products' => 'true',
+        's2s_product_map' => '{}'),
     
-    //App settings
-    'app_deleteProducts' => 'false',
-    'app_addProducts' => 'true', 
-    'app_enableSelfQuery' => 'true', 
-    'app_enableProductPush' => 'true',
+    'App settings' =>
+    array(
+        'app_delete_products' => 'false',
+        'app_add_products' => 'true', 
+        'app_enable_self_query' => 'true', 
+        'app_enable_product_push' => 'true'),
     
-    //API settings
-    'api_enabled' => 'true'
+    'API settings' =>
+    array(
+        'api_enabled' => 'true')
 );
 
 ?>
