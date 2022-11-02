89c57e7cae56fac035df68d91f3e19dee00d0d51
Keenan-Faure <keenan.asterisk@gmail.com>
2022-11-02 12:47:45 +0200

    changed naming

--- config/settings.php
+++ config/settings.php
@@ -6,26 +6,25 @@
 //on the server
 
 return array(
-
-    'Stock2Shop Credentials' =>
+    'Stock2Shop_Credentials' =>
     array(
         's2s_user' => 'keenan.faure',
         's2s_password' => 'Re_Ghoul'),
 
-    'S2S settings' =>
+    'S2S_settings' =>
     array(
         's2s_add_products' => 'true',
         's2s_delete_products' => 'true',
         's2s_product_map' => '{}'),
     
-    'App settings' =>
+    'App_settings' =>
     array(
         'app_delete_products' => 'false',
         'app_add_products' => 'true', 
         'app_enable_self_query' => 'true', 
         'app_enable_product_push' => 'true'),
     
-    'API settings' =>
+    'API_settings' =>
     array(
         'api_enabled' => 'true')
 );
