876395688d5ad5a8d04152860123215a2dc615b8
Keenan-Faure <keenan.asterisk@gmail.com>
2022-11-09 14:08:41 +0200

    updated script/css

--- Scripts/app.js
+++ Scripts/app.js
@@ -11,7 +11,7 @@ $(document).ready(()=>
         }
         else
         {
-            $('.conditions').$('.conditions').css({
+            $('.conditions').css({
                 "z-index": "-1", 
                 "opacity": "0"
             })
--- Scripts/url_woo.js
+++ Scripts/url_woo.js
@@ -6,13 +6,95 @@ document.getElementById('url').value = '';
 
 function changeUrl(value, storename)
 {
-    if(value.value == 'wooAuthenticate')
+    if(value.value == 'woo_Auth')
     {
+        hideShowData($('#post'), 'hide');
+        hideShowData($('#rmv1'), 'show');
+        hideShowData($('#rmv2'), 'show');
         urlText = 'https://' + storename + '/wc-api/v3/';
         url.value = urlText;
     }
-    if(value.value == 'getCustomer')
+    if(value.value == 'woo_getCustomer')
     {
-
+        hideShowData($('#post'), 'hide');
+        hideShowData($('#rmv1'), 'show');
+        hideShowData($('#rmv2'), 'show');
+        urlText = 'https://' + storename + '/wc-api/v3/customers/<id>';
+        url.value = urlText;
     }
+    if(value.value == 'woo_getCustomer_l')
+    {
+        hideShowData($('#post'), 'hide');
+        hideShowData($('#rmv1'), 'show');
+        hideShowData($('#rmv2'), 'show');
+        urlText = 'https://' + storename + '/wc-api/v3/customers';
+        url.value = urlText;
+    }
+    if(value.value == 'woo_deleteCustomer')
+    {
+        hideShowData($('#post'), 'hide');
+        hideShowData($('#rmv1'), 'show');
+        hideShowData($('#rmv2'), 'show');
+        urlText = 'https://' + storename + '/wc-api/v3/customers/<id>';
+        url.value = urlText;
+    }
+    if(value.value == 'woo_updateCustomer')
+    {
+        hideShowData($('#post'), 'show');
+        hideShowData($('#rmv1'), 'hide');
+        hideShowData($('#rmv2'), 'hide');
+        urlText = 'https://' + storename + '/wc-api/v3/customers/<id>';
+        url.value = urlText;
+    }
+    if(value.value == 'woo_postCustomer')
+    {
+        hideShowData($('#post'), 'show');
+        hideShowData($('#rmv1'), 'hide');
+        hideShowData($('#rmv2'), 'hide');
+        urlText = 'https://' + storename + '/wc-api/v3/customers/<id>';
+        url.value = urlText;
+    }
+}
+
+//displays or hides the containers
+//denpending on the value of the parameter
+function hideShowData(id, display)
+{
+    $(document).ready(()=>
+    {
+        if(display == 'show')
+        {
+            $(id).css(
+            {
+                "display": "block"
+            });
+            $(['#cs', '#ck']).css(
+                {
+                    "display": "block"
+                }
+            );
+            $(['#pst']).css(
+                {
+                    "display": "none"
+                }
+            );
+        }
+        else if(display == 'hide')
+        {
+            $(id).css(
+            {
+                "display": "none"
+            });
+            $(['#cs', '#ck']).css(
+                {
+                    "display": "none"
+                }
+            );
+            $(['#pst']).css(
+                {
+                    "display": "block"
+                }
+            );
+        }
+    });
 }
