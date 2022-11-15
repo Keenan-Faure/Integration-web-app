0f1fbc56393f3b191cfee1fc45daa3a305efaec6
Keenan-Faure <keenan.asterisk@gmail.com>
2022-11-15 11:57:49 +0200

    created woocommerce push script/html

---
+++ Scripts/pushWoo.js
@@ -0,0 +1,33 @@
+
+
+
+const req = async function() 
+{
+    let url = createURL();
+    const resp = await fetch(url,
+    {
+        method: 'GET', // *GET, POST, PUT, DELETE, etc.
+        mode: 'cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'include', // include, *same-origin, omit
+        headers: 
+        {
+            'Access-Control-Allow-Origin': '*',
+            'Content-Type': 'application/json'
+        },
+        redirect: 'follow', // manual, *follow, error
+        referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    });
+    const json = await resp.json();
+    console.log(json);
+}
+req();
+
+function createURL()
+{
+    arrayUrl = (document.URL).split('/');
+    url = 'http://' + arrayUrl[2] + '/' + 'cURL/pushWoocommerce.php';
+    return url;
+}
+
+
---
+++ cURL/pushWoocommerce.php
@@ -0,0 +1,76 @@
+<?php
+
+session_start();
+include('cURL.php');
+include("../createConnection.php");
+use Connection\Connection as connect;
+use cURL\CURL as curl;
+
+if($_SESSION['connection']->active == true)
+{
+    $curl = new curl();
+    $connection = new connect();
+
+    //credentials
+    $_woo_settings = $_SESSION['woo_settings'];
+
+    //creates array of products to push
+    $rawConnection = $connection->createConnection($_SESSION['connection']->credentials->username, $_SESSION['connection']->credentials->password, 'localhost', $_SESSION['connection']->credentials->dbname)->rawValue;
+    $arrayData = ($connection->converterObject($rawConnection, 'SELECT * FROM Inventory LIMIT 1')->result);
+
+    $message = 'Pushing Products... please wait';
+    if(sizeof($arrayData) <= 0)
+    {
+        $message = 'No Products found to push';
+    }
+    $sku = $arrayData[0]->SKU;
+    $found = $curl->check_woo_sku($sku);
+    $arrayData = json_encode($arrayData);
+    if($found == true)
+    {
+        //exists
+        //gets settings from php file
+        $wooSettings = $_SESSION['woo_settings'];
+        $storeName = $wooSettings->Woocommerce_Store->store_name;
+        $url = 'https://' . $storeName. '/wc-api/v3/products?filter[sku]=' . $sku;
+        $ck = $wooSettings->Woocommerce_Store->consumer_key;
+        $cs = $wooSettings->Woocommerce_Store->consumer_secret;
+
+        $result = $this->get_web_page($url, $productData, $ck, $cs, 'put');
+    }
+    else
+    {
+        //do not exist
+        //gets settings from php file
+        $wooSettings = $_SESSION['woo_settings'];
+        $storeName = $wooSettings->Woocommerce_Store->store_name;
+        $url = 'https://' . $storeName. '/wc-api/v3/products?filter[sku]=' . $sku;
+        $ck = $wooSettings->Woocommerce_Store->consumer_key;
+        $cs = $wooSettings->Woocommerce_Store->consumer_secret;
+
+        //creates new products on Woocommerce
+
+        //--Need to create product data from Woocommerce map
+        //--Need to create map in settings
+        //--As well as an internal map, if the map is not defined
+
+        $result = $this->get_web_page($url, $productData, $ck, $cs, 'post');
+    }
+
+
+
+    //decide whether to post or put the data depending on if the SKU is found in Woocommerce
+    /*
+    
+    - Run script echo from php 
+    - create recursive function to send updates to Woocommerce based on the number of
+      products that are in the array
+      - condition to check if n == 0
+      - send update, if response is 200 (okay) then update page, increment total
+      - if response if anything but 200, then add to logs and continue anyways
+    - If it is done then return a completed page, update existing page using js
+
+    */
+}
+
+?>
