fe9e2f6417dcff85f7018d678eb26a0ad7263126
Keenan-Faure <keenan.asterisk@gmail.com>
2022-11-15 17:00:30 +0200

    updated woo_settings

--- config/woo_settings.php
+++ config/woo_settings.php
@@ -11,13 +11,65 @@ return array(
         "store_name" => "s2s.ucc.co.za",
         "consumer_key" => "ck_95cdf3d8b96beb682eaa8fdac18d9c0c197751e5",
         "consumer_secret" => "cs_63105a5c9d86c4572c4542c1233f5597783b12bb"),
-
     "Woocommerce_Settings" =>
     array(
-        "woo_add_products" => "true",
-        "woo_delete_products" => "false",
+        "woo_use_product_map" => "true", //true, false
+        "woo_add_products" => "true", //true, false
+        "woo_product_status" => "publish", //can be publish, draft
+        "woo_delete_products" => "false", //true, false
         "woo_product_map" => 
-        '{}'
+        '{
+            "title": "$product->Title",
+            "type": "$product->Type",
+            "status": "$_wooSettings->Woocommerce_Settings->woo_product_status",
+            "managing_stock": false,
+            "description": "$product->Description",
+            "short_description": "",
+            "attributes": 
+            [
+                {
+                    "name": "Brand",
+                    "slug": "brand",
+                    "position": 1,
+                    "visible": true,
+                    "variation": false,
+                    "options": 
+                    [
+                        "$product->Brand"
+                    ]
+                },
+                {
+                    "name": "Product Type",
+                    "slug": "product-type",
+                    "position": 2,
+                    "visible": true,
+                    "variation": false,
+                    "options": 
+                    [
+                        "$product->Meta_1"
+                    ]
+                },
+                {
+                    "name": "Version Released",
+                    "slug": "product-version",
+                    "position": 3,
+                    "visible": true,
+                    "variation": false,
+                    "options": 
+                    [
+                        "$product->Meta_3"
+                    ]
+                }
+            ],
+            "variations": 
+            [{
+                "sku": "$product->SKU",
+                "regular_price": "$product->ComparePrice",
+                "sale_price": "$product->SellingPrice",
+                "stock_quantity": "$product->CapeTown_Warehouse",
+                "weight": "$product->Weight"
+            }]
+        }'
     )
 );
 
