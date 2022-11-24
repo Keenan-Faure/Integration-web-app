a782575270ea7dd188a55663bc6afd022f2dff61
Keenan-Faure <keenan.asterisk@gmail.com>
2022-11-24 13:33:43 +0200

    updated to include new tab/update

--- Scripts/fade.js
+++ Scripts/fade.js
@@ -4,6 +4,7 @@ $(document).ready(()=>
     $('#vDatad').click(()=>
     {
         $('#variant').fadeOut(300);
+        $('#connector').fadeOut(300);
         $('#general').fadeIn(300);
     });
 });
@@ -14,7 +15,18 @@ $(document).ready(()=>
     $('#gData').click(()=>
     {
         $('#general').fadeOut(300);
+        $('#connector').fadeOut(300);
         $('#variant').fadeIn(300);
+    });
+});
+
+$(document).ready(()=>
+{
+    $('#cData').click(()=>
+    {
+        $('#general').fadeOut(300);
+        $('#variant').fadeOut(300);
+        $('#connector').fadeIn(300);
     });
 });
 
@@ -34,6 +46,85 @@ $(document).ready(()=>
         $('.containerNew').fadeTo(500, 1);
     }, 1000);
 });
+
+$(document).ready(()=>
+{
+    setTimeout(()=>
+    {
+        req(document.querySelector('.s').innerHTML);
+    }, 200);
+});
+
+//fetch for ID's
+function createURL(sku)
+{
+    arrayUrl = (document.URL).split('/');
+    url = 'http://' + arrayUrl[2] + '/' + 'endpoints/getIDs.php?q=' + sku;
+    return url;
+}
+const req = async function(sku) 
+{
+    let url = createURL(sku);
+    const resp = await fetch(url,
+    {
+        method: 'GET', // *GET, POST, PUT, DELETE, etc.
+        mode: 'cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'include', // include, *same-origin, omit
+        headers: 
+        {
+            'Access-Control-Allow-Origin': '*',
+            'Content-Type': 'application/json'
+        },
+        redirect: 'follow', // manual, *follow, error
+        referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    });
+    const json = await resp.json();
+    addConnectorDetails(json.body[0].P_ID, json.body[0].ID, json.body[0].Pushed);
+    populateAuditTrail(json.body[0].User, json.body[0].Audit_Date);
+
+}
+
+//function to populate the ID's values
+function addConnectorDetails(p_id, v_id, s2s_active)
+{
+    if(p_id == null)
+    {
+        document.querySelector('.p_id').innerHTML = 'null';
+    }
+    else
+    {
+        document.querySelector('.p_id').innerHTML = p_id;
+    }
+    if(v_id == null)
+    {
+        document.querySelector('.v_id').innerHTML = 'null';
+    }
+    else
+    {
+        document.querySelector('.v_id').innerHTML = v_id;
+    }
+    if(s2s_active == null)
+    {
+        document.querySelector('.s2s_active').innerHTML = 'null';
+    }
+    else
+    {
+        document.querySelector('.s2s_active').innerHTML = s2s_active; 
+    }
+}
+function populateAuditTrail(user, date)
+{
+    if(user == null)
+    {
+        user = "{{null}}";
+    }
+    if(date == null)
+    {
+        date = "{{null}}";
+    }
+    document.querySelector(".auditTrail").innerHTML = "Last update on " + date + " by User: " + user;
+}
 
 
 
