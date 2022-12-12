051346532c917d4a7e66f7c5a96f9d011a45680c
Keenan-Faure <keenan.asterisk@gmail.com>
2022-12-12 16:03:53 +0200

    created search.js

---
+++ Scripts/search.js
@@ -0,0 +1,9 @@
+function search()
+{
+    let value = document.querySelector('.search-field');
+    console.log(value.value);
+}
+
+//takes value and pushes it as a url to endpoint
+    //getProductsSearch.php
+//endpoint returns data and it is displayed on screen
