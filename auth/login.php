ccc02559f34f3b355abb0240b9deb8d9477d0b6a
Keenan Faure <97687673+Keenan-Faure@users.noreply.github.com>
2023-01-11 12:14:03 +0200

    adjusted login styles

--- auth/login.php
+++ auth/login.php
@@ -126,24 +126,23 @@
     </head>
     <body>
         <div class = 'background'>
-        <div class = 'background-cover'></div>
-            <h1 id='header1'>Login to MySql Server</h1>
-            <div class='line' id='line'></div>
-            <div class="modalContainer">
-                <form method='post' action='../bin/controllers/connect.php'>
-                    <input type='text' autocomplete="off" name='uname' placeholder='Enter Username' id='f1' required>
-                    <br><br>
-                    <input type='password' autocomplete="off" name='psw' placeholder='Enter Password' id='f2'>
-                    <br><br>
-                    <input type='text' name='host' placeholder='Localhost' id='f3' readonly>
-                    <br><br>
-                    <a class='register' href='register.php'>Click here to register</a>
-                    <br><br>
-                    <input class = 'button' type='submit' id='f4'>
-                </form>
+            <div class='modal'>
+                <h1 id='header1'>Login</h1>
+                <hr class='line'>
+                <div class="modalContainer">
+                    <form method='post' action='../bin/controllers/connect.php'>
+                        <input type='text' autocomplete="off" name='uname' placeholder='Enter Username' id='f1' required>
+                        <br><br>
+                        <input type='password' autocomplete="off" name='psw' placeholder='Enter Password' id='f2'>
+                        <br><br>
+                        <input type='text' name='host' placeholder='Localhost' id='f3' readonly>
+                        <br><br><br>
+                        <a class='register' href='register.php'>Click here to register</a>
+                        <br><br>
+                        <input class = 'button' type='submit' id='f4'>
+                    </form>
                 </div>
             </div>
-        </div>
         </div>
     </body>
 </html>
